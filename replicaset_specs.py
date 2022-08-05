8526d86d9c690781b2595fbc4fb959b6bfe2902a
Martin Canovas <MartinCanovas@users.noreply.github.com>
2022-08-04 22:41:37 -0300

    Update replicaset_specs.py

--- replicaset_specs.py
+++ replicaset_specs.py
@@ -9,25 +9,26 @@ def replicaset_specs(file):
         if jsonFile:
             listOfSpecs = {}
             for item in jsonFile:
-                hostname  = item['hostInfo']['system']['hostname']
-                numCores  = 'numCores' + ' ' + str(item['hostInfo']['system']['numCores'])
-                memSizeGB = str(round(float(item['hostInfo']['system']['memSizeMB']) / 1024, 2)) + 'GB'
-                osName    = item['hostInfo']['os']['name']
-                osVersion = item['hostInfo']['os']['version']
-                listOfSpecs[hostname] = [numCores, memSizeGB, osName, osVersion]
+                hostname   = item['hostInfo']['system']['hostname']
+                numCores   = 'numCores' + ' ' + str(item['hostInfo']['system']['numCores'])
+                memSizeGB  = str(round(float(item['hostInfo']['system']['memSizeMB']) / 1024, 2)) + 'GB'
+                memLimitMB = str(round(float(item['hostInfo']['system']['memLimitMB']) / 1024, 2)) + 'GB'
+                osName     = item['hostInfo']['os']['name']
+                osVersion  = item['hostInfo']['os']['version']
+                listOfSpecs[hostname] = [numCores, memSizeGB, memLimitMB, osName, osVersion]
+
+            for hostname in sorted(listOfSpecs, key=listOfSpecs.get):
+                print(hostname,':', listOfSpecs[hostname])
         else:
             print('file', file, 'is empty')
-            return
-
-    for hostname in sorted(listOfSpecs, key=listOfSpecs.get):
-        print(hostname,':', listOfSpecs[hostname])
+    return
 
 if __name__ == '__main__':
     directory = '.'
     listOfFiles = []
-    for path, subdirs, files in os.walk(directory):
+    for root, dirs, files in os.walk(directory):
         for name in files:
-            listOfFiles.append(os.path.join(path, name))
-            for file in listOfFiles:
-                if (file.find('lastPing') != -1):
-                    replicaset_specs(file)
+            listOfFiles.append(os.path.join(root, name))
+    for file in listOfFiles:
+        if (file.find('lastPing') != -1):
+            replicaset_specs(file)
